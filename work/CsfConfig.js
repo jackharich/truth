15e643c8da21911c8c7194c66ba98c8ac7d6e5c4
Jack Harich <register@thwink.org>
2018-06-27 17:27:25 -0400

    xxx

--- work/CsfConfig.js
+++ work/CsfConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // databaseDialect:  'mysql', TEST
+    // databaseDialect:  'mysql', TESTxxx
 
     tokenSecret:      'msdafkjvxzc768!@$%GFDgdf87adfsgmn54r23q87u', // Random keyboard typing.
     passwordSecret:   'ijo^&654^$(&6xw808gRf765%$365675jhbvjhs'  // Random keyboard typing.
